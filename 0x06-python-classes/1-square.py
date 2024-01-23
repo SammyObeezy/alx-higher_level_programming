fdcc062a1f5d09d544d71a7ac183109c4a64ed57
SammyObeezy <barasa07@gmail.com>
2024-01-23 12:24:10 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
-"""A class that defines a square by its size.
 
-    Attributes:
-        __size (int): The size of the square (private attribute).
-    """
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
-        """Initialize a new Square instance with a given size.
+        """Initialize a new Square.
 
         Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
-
-    def get_size(self):
-         """Get the size of the square.
-
-        Returns:
-            int: The size of the square.
-        """
-        return self.__size
-
-    def set_size(self, size):
-        """Set the size of the square.
-
-        Args:
-            size (int): The new size of the square.
+            size (int): The size of the new square.
         """
         self.__size = size
