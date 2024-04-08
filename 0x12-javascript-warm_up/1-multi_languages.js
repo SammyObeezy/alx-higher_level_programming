95a3392c783c590f74c14a5a6d41cc52db92a32a
SammyObeezy <barasa07@gmail.com>
2024-04-08 10:03:21 -0700

    Intro to js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const x = "C is fun";
+const y = "Python is cool";
+const z = "Javascript is amazing";
+
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+Just an introduction to Javascript
