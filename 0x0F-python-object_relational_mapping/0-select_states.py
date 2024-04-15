dbc5d483462de4b11a1a7e8ef8cfcf0ee3df58f3
SammyObeezy <barasa07@gmail.com>
2024-04-15 05:20:18 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,20 +3,32 @@
 import MySQLdb
 from sys import argv
 
-#The following code should not be executed when imported
-if __name__=='__main__':
 
-    #make a connection to the database now
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+def main():
+    # The following code should not be executed when imported
+    if __name__ == '__main__':
+        # Make a connection to the database now
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
 
-    #This gives us the ability to have separate multiple working environments
-    #With the same connection to the database
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+        # This gives us the ability to have separate multiple working environments
+        # with the same connection to the database
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states")
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    #Clean up the entire process
-    cur.close()
-    db.close()
+        rows = cur.fetchall()
+        for i in rows:
+            print(i)
+
+        # Clean up the entire process
+        cur.close()
+        db.close()
+
+
+if __name__ == '__main__':
+    main()
