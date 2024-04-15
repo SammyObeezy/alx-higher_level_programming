5d0553e290a6e10e4d6b4ea69057f6f2a5569641
SammyObeezy <barasa07@gmail.com>
2024-04-15 05:03:49 -0700

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a script that lists all states from DB htb_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+#The following code should not be executed when imported
+if __name__=='__main__':
+
+    #make a connection to the database now
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],passwd=argv[2], db=argv[3])
+
+    #This gives us the ability to have separate multiple working environments
+    #Wit the same connection to the database
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    #clean up the entire process
+    cur.close()
+    db.close()
