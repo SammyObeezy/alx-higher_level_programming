93f7f20c1df7b6e1cbef2d57176d239ae207f192
SammyObeezy <barasa07@gmail.com>
2024-04-15 05:22:42 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""This is a script that lists all states from DB htb_0e_0_usa"""
+""" Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 from sys import argv
 
-
-def main():
-    # The following code should not be executed when imported
-    if __name__ == '__main__':
-        # Make a connection to the database now
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-        )
-
-        # This gives us the ability to have separate multiple working environments
-        # with the same connection to the database
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states")
-
-        rows = cur.fetchall()
-        for i in rows:
-            print(i)
-
-        # Clean up the entire process
-        cur.close()
-        db.close()
-
-
+# The code should not be executed when imported
 if __name__ == '__main__':
-    main()
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
