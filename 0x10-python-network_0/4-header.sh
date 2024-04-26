92006ede91f5b37d8252bcc97b959d5dfd75a387
SammyObeezy <barasam07@gmail.com>
2024-04-26 02:28:00 -0700

    Some bug

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to send custom headers to the servers
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to post data (url-encoded) to a given server
-curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
